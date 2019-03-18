acbd9a48e307ffee0d887c0ca3c5b25893bdaa93
Alexandre <alex.apps.12@gmail.com>
2019-03-18 19:26:21 +0100

    added readme

---
+++ main.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+var isCalling = false;
+var currentMessage = '';
+var responseMessage = '';
+
+app.get('/isCalling', (req, res) => {
+    res.end(`${isCalling}/${currentMessage}`);
+});
+
+app.get('/setCalling', (req, res) => {
+    isCalling = true;
+    currentMessage = req.query.message;
+    res.end('200');
+});
+
+app.get('/answerCall', (req, res) => {
+    isCalling = false;
+    responseMessage = req.query.rmessage;
+    currentMessage = '';
+    res.end('200');
+});
+*
+app.get('/stopCalling', (req, res) => {
+    isCalling = false;
+    currentMessage = '';
+    responseMessage = '';
+    res.end('200');
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
